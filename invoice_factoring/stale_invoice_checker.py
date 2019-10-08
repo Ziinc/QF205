495d6b125594123666f09d663b4b03d4ad263557
ziinc <tzeyiing@gmail.com>
2019-10-08 19:09:12 +0800

    updated function

--- invoice_factoring/stale_invoice_checker.py
+++ invoice_factoring/stale_invoice_checker.py
@@ -1,4 +1,11 @@
-def stale_invoice(x):
-  if x > 30:
-    
+"""
+Checks the staleness of an invoice
+
+"""
+
+def check_stale_invoice(invoice_date, staleness_threshold=30):
+  if staleness_threshold > 30:
+    return True
+  
+  return False
   
