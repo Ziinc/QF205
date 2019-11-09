532216afbfb7d2188d10459d0ff52faa34cff004
ziinc <tzeyiing@gmail.com>
2019-11-09 11:48:33 +0800

    implemented stale invoice checking

--- app/model/invoice_factoring.py
+++ app/model/invoice_factoring.py
@@ -1,3 +1,4 @@
+import datetime
 
 
 def calc_amt_to_collect(invoice):
@@ -10,20 +11,24 @@ def calc_amt_to_collect(invoice):
 def calc_factor_amt(invoice_value, LTV):
     """
     Calculates the factor amount for the invoice (a/c rec)
+    Amt given to the supplier
     """
     factor_amnt = invoice_value*(LTV/100)
     str_amnt = str(round(factor_amnt))
     number_of_digits = len(str_amnt)
     number_to_divide_multiply = 10**number_of_digits
-    round_number = round(factor_amnt/number_to_divide_multiply,2)
+    round_number = round(factor_amnt/number_to_divide_multiply, 2)
     return int(round_number*number_to_divide_multiply)
 
-def check_stale_invoice(invoice_date, staleness_threshold=30):
+
+def check_stale_invoice(invoice, staleness_threshold=30):
     """
-    Checks the staleness of an invoice
+    Checks the staleness of an invoices
 
     """
-    if staleness_threshold > 30:
+    now = datetime.datetime.now()
+    delta = invoice.date - now
+    day_diff = delta.days
+    if day_diff <= staleness_threshold:
         return True
-
     return False
