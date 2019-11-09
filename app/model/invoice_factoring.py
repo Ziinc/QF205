b08b3ba27fa9bf1622f8e52800c10f28cc244f5c
ziinc <tzeyiing@gmail.com>
2019-11-09 14:58:06 +0800

    added docs, moved due date to separate function

--- app/model/invoice_factoring.py
+++ app/model/invoice_factoring.py
@@ -4,11 +4,11 @@ import datetime
 def calc_amt_to_collect(invoice, interest_pct=0.12):
     """
     Calculates the total interest+principal to collect.
+    Args:
+    - invoice (Invoice)
+    - interest_pct (float)
     """
     factor_amt = calc_factor_amt(invoice.amt, invoice.factor_pct)
-
-    # due_date = invoice.factor_start_date + \
-    #     datetime.timedelta(days=invoice.credit_term)
 
     now = datetime.date.today()
     from_factor_start_delta = invoice.factor_start_date - now
@@ -17,23 +17,30 @@ def calc_amt_to_collect(invoice, interest_pct=0.12):
     return factor_amt + interest
 
 
+def calc_due_date(invoice):
+    # due_date = invoice.factor_start_date + \
+    #     datetime.timedelta(days=invoice.credit_term)
+    pass
+
+
 def calc_factor_amt(invoice_value, ltv):
     """
-    Calculates the factor amount for the invoice (a/c rec)
-    Amt given to the supplier
+    Calculates the factor amount for the invoice
+
+    Args:
+    - invoice_value (float)
+    - ltv (float) 
     """
     return invoice_value*ltv
-    # str_amnt = str(round(factor_amnt))
-    # number_of_digits = len(str_amnt)
-    # number_to_divide_multiply = 10**number_of_digits
-    # round_number = round(factor_amnt/number_to_divide_multiply, 2)
-    # return int(round_number*number_to_divide_multiply)
 
 
 def check_stale_invoice(invoice, staleness_threshold=30):
     """
     Checks the staleness of an invoices
 
+    Args:
+    - invoice (Invoice)
+    - staleness_threshold (int)
     """
     now = datetime.date.today()
     delta = invoice.date - now
