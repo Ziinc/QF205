146da7fbb16ae2a2fa67f2b26958f57208c81f79
ziinc <tzeyiing@gmail.com>
2019-11-09 18:23:50 +0800

    Able to launch file now

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ Entrypoint for the app.
 
 from app import App
 import sys
-from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget
+from PyQt5.QtWidgets import QMainWindow, QApplication
 from PyQt5 import uic
 from app.controller import Controller
 
@@ -12,38 +12,14 @@ qtCreatorFile = "app/LoanFactoringUI.ui"
 Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)
 
 
-class Main(QMainWindow,  Ui_MainWindow):
-    def init(self):
-        super().__init__()
+class Main(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super(Main, self).__init__()
         self.setupUi(self)
-        con = Controller()
-
-    #     self.label_RepaymentPeriod.setText(
-    #         "<span style='font-size:18pt;'>Repayment Period (In Years)</span>"
-    #         "<span style='font-size:12pt;'>  (Between 1-25 years)</span>")
-
-    #     self.pushButton_Calculate.clicked.connect(self.PB_C)
-
-    # def PB_C(self,):
-    #     PV = float(self.lineEdit_LoanAmount.text())
-    #     t = float(self.lineEdit_RepaymentPeriod.text())
-    #     r = float(self.lineEdit_InterestRateOfLoan.text())/100
-    #     P = math.ceil((r*PV/12.0)/(1-(1+r/12.0)**(-12*t)))
-    #     self.lineEdit_MonthlyInstallment.setText(str(P))
 
 
 if __name__ == '__main__':
-
     app = QApplication(sys.argv)
-
     main = Main()
     main.show()
     sys.exit(app.exec_())
-
-
-# if __name__ == "__main__":
-
-#     app = QApplication(sys.argv)
-#     app = App()
-#     app.show()
-#     sys.exit(app.exec_())
