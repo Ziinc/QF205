e02534dd101347921eacd14c9a5eac840ab929b1
Darren <darrenfongdj@gmail.com>
2019-11-09 11:19:59 +0800

    Fixed indent

--- app/model/invoice_factoring.py
+++ app/model/invoice_factoring.py
@@ -11,12 +11,12 @@ def calc_factor_amt(invoice_value, LTV):
     """
     Calculates the factor amount for the invoice (a/c rec)
     """
-  factor_amnt = invoice_value*(LTV/100)
-  str_amnt = str(round(factor_amnt))
-  number_of_digits = len(str_amnt)
-  number_to_divide_multiply = 10**number_of_digits
-  round_number = round(factor_amnt/number_to_divide_multiply,2)
-  return int(round_number*number_to_divide_multiply)
+    factor_amnt = invoice_value*(LTV/100)
+    str_amnt = str(round(factor_amnt))
+    number_of_digits = len(str_amnt)
+    number_to_divide_multiply = 10**number_of_digits
+    round_number = round(factor_amnt/number_to_divide_multiply,2)
+    return int(round_number*number_to_divide_multiply)
 
 def check_stale_invoice(invoice_date, staleness_threshold=30):
     """
