0ae3aba2830f01aa6fba6581fff8b9449cb64a55
ziinc <tzeyiing@gmail.com>
2019-11-10 16:04:28 +0800

    added test for invoice amt parsing

--- test/invoice_test.py
+++ test/invoice_test.py
@@ -11,12 +11,22 @@ class TestInvoiceInit(unittest.TestCase):
         self.assertEqual(
             invoice.date, datetime.date.fromisoformat('2012-12-12'))
 
+    def test_input_parsing(self):
+        try:
+            Invoice("20,200,000", "2012-12-12", 4, 0.80)
+            Invoice("$20,200,000", "2012-12-12", 4, 0.80)
+            Invoice("$20,200,000", "2012-12-12", 4, 0.80)
+        except Exception as e:
+            self.fail(f"Failed invoice insertion. Error: {e}")
+
     def test_invoice_init_error(self):
         # check for invoice accepted values
         self.assertRaises(AttributeError, Invoice,
                           None, "2012-12-12", 4, 0.80)
         self.assertRaises(AttributeError, Invoice,
-                          "$5", "2012-12-12", 4, 0.80)
+                          "$,5", "2012-12-12", 4, 0.80)
+        self.assertRaises(AttributeError, Invoice,
+                          "$,5.00", "2012-12-12", 4, 0.80)
 
         # check invoice datestring
         self.assertRaises(AttributeError, Invoice,
